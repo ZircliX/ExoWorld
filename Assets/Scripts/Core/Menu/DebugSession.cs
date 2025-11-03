5f01970a137cb5bdd59231e7826ff8b7246edd09
Dydou <MegaFight63@laposte.net>
2025-11-03 16:26:19 +0100

    Debug Fix

--- Assets/Scripts/Core/Menu/DebugSession.cs
+++ Assets/Scripts/Core/Menu/DebugSession.cs
@@ -1,8 +1,11 @@
 ﻿using OverBang.GameName.Core.Metrics;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Multiplayer;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -36,6 +39,26 @@ namespace OverBang.GameName.Core.Menu
 
         private async void StartSession()
         {
+            try
+            {
+                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(4);
+                
+                
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(
+                    allocation.RelayServer.IpV4,
+                    (ushort) allocation.RelayServer.Port,
+                    allocation.AllocationIdBytes,
+                    allocation.Key,
+                    allocation.ConnectionData
+                );
+
+                NetworkManager.Singleton.StartHost();
+            }
+            catch (RelayServiceException e)
+            {
+                Debug.Log(e);
+            }
+            
             SessionOptions options = new SessionOptions()
             {
                 Name = sessionId,
@@ -44,7 +67,18 @@ namespace OverBang.GameName.Core.Menu
             };
 
             session = await MultiplayerService.Instance.CreateOrJoinSessionAsync(sessionId, options);
+            if (session.PlayerCount == 0)
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+            else
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+            
             Debug.Log("Session started with ID: " + session.Id);
+            Debug.Log(GameMetrics.Global.SceneCollection.HubSceneRef.Name);
+            Debug.Log(NetworkManager.Singleton.SceneManager);
             NetworkManager.Singleton.SceneManager.LoadScene(GameMetrics.Global.SceneCollection.HubSceneRef.Name, LoadSceneMode.Single);
         }
     }
