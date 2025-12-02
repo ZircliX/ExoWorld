139983695738ba1d804c0e051ceec8e37c66c6c1
Dydou <MegaFight63@laposte.net>
2025-12-02 13:12:24 +0100

    Package (behavior tree) + début system Enemy

--- Assets/Scripts/Gameplay/Enemies/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,15 +1,18 @@
-﻿using KBCore.Refs;
+﻿using System;
+using KBCore.Refs;
 using OverBang.GameName.Core;
 using OverBang.Pooling;
 using OverBang.Pooling.Resource;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace OverBang.GameName.Gameplay
 {
     public class Enemy : NetworkBehaviour, IPoolInstanceListener
     {
         [field : SerializeField, Self] public NetworkObject EnemyNetworkObject { get; private set; }
+        [field : SerializeField, Self] public NavMeshAgent NavMeshAgent { get; private set; }
         [field : SerializeField] public Transform enemyModelContainer { get; private set; }
         [SerializeField] public EnemyData enemyData;
         public IPool Pool { get; protected set; }
@@ -22,8 +25,20 @@ namespace OverBang.GameName.Gameplay
         public void Initialize(string enemyDataId)
         {
             SetEnemyModelRpc(enemyDataId);
+            
+            if (IsOwner)
+            {
+                EnemyManager.Instance.Register(this);
+            }
         }
-        
+
+        private void Update()
+        {
+            if (!IsOwner) return;
+
+            //NavMeshAgent.SetDestination();
+        }
+
         public void OnSpawn(IPool pool)
         {
             Pool = pool;
@@ -32,7 +47,6 @@ namespace OverBang.GameName.Gameplay
         
         public void OnDespawn(IPool pool)
         {
-            
         }
 
         [Rpc(SendTo.Everyone)]
@@ -52,7 +66,7 @@ namespace OverBang.GameName.Gameplay
 
         public void OnDeath()
         {
-            
+            EnemyManager.Instance.Unregister(this);
         }
         
         //LOGIQUE DE DEPLACEMENT ET TOUT ET TOUT.
--- Assets/Scripts/Gameplay/Enemies/EnemyManager.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyManager.cs
@@ -1,9 +1,25 @@
-﻿using Helteix.Singletons.SceneServices;
-
-namespace OverBang.GameName.Gameplay
-{
-    public class EnemyManager 
-    {
+﻿using System.Collections.Generic;
+using Helteix.Singletons.SceneServices;
+ 
+ namespace OverBang.GameName.Gameplay
+ {
+     public class EnemyManager : SceneService<EnemyManager>
+     {
+        private List<Enemy> enemies;
         
-    }
-}
+         public void Register(Enemy enemy)
+         {
+             enemies.Add(enemy);
+         }
+
+         public void Unregister(Enemy enemy)
+         {
+             enemies.Remove(enemy);
+         }
+         
+         
+         
+         
+         
+     }
+ }
