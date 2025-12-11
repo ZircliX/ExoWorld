c3a35ebca5baa6f970adfb7e3204257a0c73d630
ZircliX <lolofy71@gmail.com>
2025-12-11 01:49:13 +0100

    Fix quest ui

--- Assets/Scripts/Gameplay/Enemies/Spawn/EnemySpawnerManager.cs
+++ Assets/Scripts/Gameplay/Enemies/Spawn/EnemySpawnerManager.cs
@@ -66,7 +66,7 @@ namespace OverBang.GameName.Gameplay
                 int currentSpawnedEnemies = 0;
 
                 HashSet<Area> spawnableZones = AreaManager.Instance.GetSpawnableAreas();
-                Debug.Log($"Wave {currentWave} started with {spawnableZones.Count} areas");
+                //Debug.Log($"Wave {currentWave} started with {spawnableZones.Count} areas");
 
                 //Single wave loop
                 while (currentSpawnedEnemies <= enemyToSpawnInWave && IsWaving)
@@ -88,7 +88,7 @@ namespace OverBang.GameName.Gameplay
                         previousSpawnableAreas = spawnableAreas;
                 }
 
-                Debug.Log($"Wave{currentWave} spawned, waiting {enemySpawnScenario.TimeBetweenWaves} for the next one !");
+                //Debug.Log($"Wave{currentWave} spawned, waiting {enemySpawnScenario.TimeBetweenWaves} for the next one !");
                 await Awaitable.WaitForSecondsAsync(enemySpawnScenario.TimeBetweenWaves);
                 currentWave++;
             }
