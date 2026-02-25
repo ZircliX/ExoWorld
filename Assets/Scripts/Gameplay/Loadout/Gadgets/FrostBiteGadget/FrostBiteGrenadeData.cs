51b9c8bdcf70d01e9128801052d7f464759f0975
Dydou <MegaFight63@laposte.net>
2026-02-25 16:21:49 +0100

    FrostBiteGrenade 100% network friendly

--- Assets/Scripts/Gameplay/Loadout/Gadgets/FrostBiteGadget/FrostBiteGrenadeData.cs
+++ Assets/Scripts/Gameplay/Loadout/Gadgets/FrostBiteGadget/FrostBiteGrenadeData.cs
@@ -1,30 +1,40 @@
-﻿using System;
-using OverBang.ExoWorld.Core.Abilities.Gadgets;
-using OverBang.ExoWorld.Core.Damage;
-using OverBang.ExoWorld.Core.Database;
-using Sirenix.OdinInspector;
-using Unity.Netcode;
-using UnityEditor;
-using UnityEngine;
+﻿    using System;
+    using OverBang.ExoWorld.Core.Abilities.Gadgets;
+    using OverBang.ExoWorld.Core.Damage;
+    using OverBang.ExoWorld.Core.Database;
+    using Sirenix.OdinInspector;
+    using Unity.Netcode;
+    using UnityEngine;
 
-namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
-{
-    [CreateAssetMenu(menuName =  "OverBang/Gadgets/FrostBiteGrenadeData")]
-    public class FrostBiteGrenadeData : GadgetData, IDatabaseAsset
+    namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
     {
-        [field: SerializeField] public NetworkObject Prefab { get; private set; }
-        [field: SerializeField] public DamageData DamageData { get; private set; }
-        [field: SerializeField] public float ThrowForce { get; private set; }
-        [field: SerializeField] public float SlowDuration { get; private set; }
-        [field: SerializeField] public float SlowPercentage { get; private set; }
-        [field : SerializeField, ReadOnly] public string ID { get; private set; }
-
-        private void OnValidate()
+        [CreateAssetMenu(menuName =  "OverBang/Gadgets/FrostBiteGrenadeData")]
+        public class FrostBiteGrenadeData : GadgetData, IDatabaseAsset
         {
-            if (string.IsNullOrEmpty(ID))
+            [field: SerializeField] public NetworkObject Prefab { get; private set; }
+            [field: SerializeField] public DamageData DamageData { get; private set; }
+            [field: SerializeField] public float ThrowForce { get; private set; }
+            [field: SerializeField] public float SlowDuration { get; private set; }
+            [field: SerializeField] public float SlowPercentage { get; private set; }
+            [field : SerializeField, ReadOnly] public string ID { get; private set; }
+
+            private void OnValidate()
+            {
+                if (string.IsNullOrEmpty(ID))
+                {
+                    ID = Guid.NewGuid().ToString();
+                    Debug.LogWarning($"[OnValidate] Regenerating ID for {name}..." +
+                                     $"{ID}");
+                }
+            }
+            
+            [Button]
+            private void GenerateID()
             {
                 ID = Guid.NewGuid().ToString();
+                Debug.LogWarning($"[OnGenerateID] ID  manually generated for {name}..." +
+                                 $"{ID}");
             }
+            
         }
     }
-}
--- Assets/Scripts/Gameplay/Loadout/Gadgets/FrostBiteGadget/FrostBiteGrenadeEntity.cs
+++ Assets/Scripts/Gameplay/Loadout/Gadgets/FrostBiteGadget/FrostBiteGrenadeEntity.cs
@@ -41,7 +41,8 @@ namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
         public void Initialize(FrostBiteGrenadeData data, FrostBiteGrenade grenade)
         {
             Data = data;
-            InitializeRpc(data.ID);
+            Debug.Log($"Owner : {Data.ID}");
+            InitializeRpc(Data.ID);
             strategy = new CryoExplosion(Data.DamageData, Data.SlowDuration, Data.SlowPercentage);
             frostBiteGrenade = grenade;
             strategy.OnExploded += OnExploded;
@@ -50,6 +51,8 @@ namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
         [Rpc(SendTo.Everyone)]
         private void InitializeRpc(string dataId)
         {
+            Debug.Log($"Database null ? {GameDatabase.Global == null}");
+            Debug.Log($"Database assets count : {GameDatabase.Global?.DatabaseAssets.Length}");
             if (GameDatabase.Global.TryGetAssetByID(dataId, out FrostBiteGrenadeData asset))
             {
                 Data = asset;
@@ -114,7 +117,7 @@ namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
                     transform.position,
                     Quaternion.identity
                 );
-                PlayVfxRpc();
+                PlayVfxRpc(vfx.NetworkObjectId);
             }
             
             if (terminated)
@@ -123,30 +126,41 @@ namespace OverBang.ExoWorld.Gameplay.Loadout.FrostBiteGadget
                 End();
             }
         }
-
+        
         [Rpc(SendTo.Everyone)]
-        private void PlayVfxRpc()
+        private void PlayVfxRpc(ulong networkObjectId)
         {
-            BroAudio.Play(Data.SoundID);
+            if (!NetworkManager.Singleton.SpawnManager.SpawnedObjects
+                    .TryGetValue(networkObjectId, out NetworkObject netObj))
+            {
+                Debug.LogError($"VFX with ID {networkObjectId} not found.");
+                return;
+            }
+            
+            vfx = netObj;
             if (vfx.TryGetComponent(out ParticleSystem ps))
             {
                 ps.Play();
                 Invoke(nameof(DestroyVfx), ps.main.duration);
             }
-        }
 
+            BroAudio.Play(Data.SoundID);
+        }
         private void DestroyVfx()
         {
+            if(!IsOwner) return;
             if(vfx != null) vfx.Despawn();
         }
         private void End()
         {
+            if(!IsOwner) return;
             frostBiteGrenade.End();
             no.Despawn();
         }
 
         public void Discard()
         {
+            if(!IsOwner) return;
             no.Despawn();
         }
 
