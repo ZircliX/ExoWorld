4d81c010d573b0eab51ae8cc78ba983584bb00c0
Maéna <maenabouet@gmail.com>
2026-03-11 10:17:12 +0100

    AnimUI MissionCompleted WIP 2

--- Assets/Scripts/Core/Manena/TestMaenaDoTween.cs
+++ Assets/Scripts/Core/Manena/TestMaenaDoTween.cs
@@ -13,29 +13,45 @@ namespace OverBang.ExoWorld.Core.Manena
         [SerializeField] private TMP_Text questCompleteTitleText;
         [SerializeField] private TMP_Text questCompleteNameText;
         [SerializeField] private TMP_Text questCompleteRewardText;
-        [SerializeField] private Ease easingCurve;
         
         [SerializeField, Range(0,1)] private float openingXScaleDuration;
         [SerializeField, Range(0,1)] private float openingYScaleDuration;
         [SerializeField] private float openingDuration;
+        [SerializeField] private Ease openingEasingCurve;
 
+        [SerializeField, Range(0,1)] private float endingXScaleDuration;
+        [SerializeField, Range(0,1)] private float endingYScaleDuration;
+        [SerializeField] private float endingDuration;
+        [SerializeField] private Ease endingEasingCurve;
 
         public void DoAnimation()
         {
             Sequence sequence = DOTween.Sequence();
             questCompleteCanvasGroup.transform.localScale = new Vector3(0.01f, 0, 1);
             
+            //Show Quest Completed BG
             sequence.Append(questCompleteCanvasGroup.DOFade(1, openingDuration));
-            sequence.Join(questCompleteCanvasGroup.transform.DOScaleY(1, openingYScaleDuration)).SetEase(easingCurve);
+            sequence.Join(questCompleteCanvasGroup.transform.DOScaleY(1, openingYScaleDuration)).SetEase(openingEasingCurve);
             
-            sequence.Append(questCompleteCanvasGroup.transform.DOScaleX(1, openingXScaleDuration)).SetEase(easingCurve);
+            sequence.Append(questCompleteCanvasGroup.transform.DOScaleX(1, openingXScaleDuration)).SetEase(openingEasingCurve);
             
-            sequence.Append(questCompleteTitleText.DOFade(1, 0.1f));
-            sequence.Join(questCompleteNameText.DOFade(1, 0.1f));
-            sequence.AppendInterval(3f);
+            //Show Quest Completed Text
+            sequence.Append(questCompleteTitleText.DOFade(1, 0.3f));
+            sequence.Join(questCompleteNameText.DOFade(1, 0.2f));
+            
+            sequence.AppendInterval(2f);
 
-            sequence.Append(questCompleteRewardText.DOFade(1, 0.1f));
-            sequence.Append(questRewardCanvasGroup.DOFade(1, 0.1f));
+            sequence.Append(questCompleteRewardText.DOFade(1, 0.2f));
+            sequence.Append(questRewardCanvasGroup.DOFade(1, 0.2f));
+            
+            //Wait
+            sequence.AppendInterval(5f);
+            
+            //Remove Quest Completed BG
+            sequence.Append(questCompleteCanvasGroup.transform.DOScaleX(0.01f, endingXScaleDuration)).SetEase(endingEasingCurve);
+            
+            sequence.Append(questCompleteCanvasGroup.transform.DOScaleY(0, endingYScaleDuration)).SetEase(endingEasingCurve);
+            sequence.Join(questCompleteCanvasGroup.DOFade(0, endingDuration));
             
             sequence.Play();
             
